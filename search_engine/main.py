df72b2e9de5cd27929fecd3df67ccc5c2a260977
EneaZer0 <miguezsergio22@gmail.com>
2020-04-24 19:45:21 +0100

    Implement gitingnore and get links for black list

--- search_engine/main.py
+++ search_engine/main.py
@@ -2,12 +2,12 @@ import URLS
 import senderDB
 
 # Commands to update information without affecting db
-# URLS.get_updated_information_covid19facts()
-# URLS.get_updated_information_who()
+#URLS.get_updated_information_covid19facts()
+#URLS.get_updated_information_who()
 
 # This are the commands to update all the covid19facts information and get it all displayed
 # senderDB.update_covid19facts()
-# senderDB.select_information('TEST')
+# senderDB.select_all('TEST')
 
 # Wikipedia black list
 URLS.getBlackListWk()
