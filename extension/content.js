0c0afa1a8e087ec12c0f2d4b33276d137adb90aa
JustasZJ <zjustas2000@gmail.com>
2020-04-24 18:23:25 +0300

    very basic plugin skeleton

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,5 @@
 var titles = document.getElementsByTagName("h3");
-var i;
-for (i = 0; i < titles.length; i++) {
+for (var i = 0; i < titles.length; i++) {
 	var percentage = Math.floor(Math.random() * 100);
 	var rating = "<br> Trustworhiness: " + percentage.toString(10) + "%";
 	if (percentage > 50) {
