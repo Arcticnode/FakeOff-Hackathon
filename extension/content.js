5f887ad829ec70572c055437025fb0278721bb14
JustasZJ <zjustas2000@gmail.com>
2020-04-25 01:33:03 +0300

    text selection

--- extension/content.js
+++ extension/content.js
@@ -1,14 +1,27 @@
-var titles = document.getElementsByTagName("h3");
-for (var i = 0; i < titles.length; i++) {
-	var div = titles[i].parentNode.parentNode;
-	if (div.className == "r") {
-		var percentage = Math.floor(Math.random() * 100);
-		var rating = "<br> Trustworhiness: " + percentage.toString(10) + "%";
-		if (percentage > 50) {
-			rating = rating.fontcolor("green");
-		} else {
-			rating = rating.fontcolor("red");
-		}
-		titles[i].innerHTML += rating;
-	}
+function highlightHandler(e) {
+    // get the highlighted text
+    var text = document.getSelection();
+    // check if anything is actually highlighted
+    var str = text.toString();
+    var words = 0;
+    for (var i = 0; i < str.length; i++) {
+    	if (str.charAt(i) != ' ') {
+    		words++;
+    		var j = i;
+    		while (j < str.length && str.charAt(j) != ' ') {
+    			j++;
+    		}
+    		i = j;
+    	}
+    }
+    if (words > 1) {
+        // we've got a highlight, now do your stuff here
+        doStuff(text);
+    }
+}
+
+document.onmouseup = highlightHandler;
+
+function doStuff(text) {
+	alert(text);
 }
