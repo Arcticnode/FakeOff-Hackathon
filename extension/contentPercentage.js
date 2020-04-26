882bf3ec19f38b57265da498079079b4df1451b6
JustasZJ <zjustas2000@gmail.com>
2020-04-26 23:32:34 +0300

    submit text

--- extension/contentPercentage.js
+++ extension/contentPercentage.js
@@ -24,7 +24,6 @@ for (var i = 0; i < titles.length; i++) {
                 rating = rating.fontcolor("orange");
             }
             titles[i].innerHTML += rating;
-          
         };
 
         req.send(null);}
--- extension/eventPage.js
+++ extension/eventPage.js
@@ -20,7 +20,20 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
 	    	}
 	    }
 	    if (words > 1) {
-			alert(clickData.selectionText);
+        	var newURL = "http://35.197.239.192/textSearch/";
+        	// var newURL = "https://www.stackoverflow.com";
+  			chrome.tabs.create({ url: newURL });
+
+  			chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  				sendResponse(str);
+  			});
+  			// chrome.runtime.sendMessage("Hi");
+  	// 		chrome.tabs.query({currentWindow: true, active: true}, 
+			// function(tabs) {
+			// 	chrome.tabs.sendMessage(tabs[0].id, "pls change the html")
+			// })
+  			// console.log("hehe");
+			// alert(clickData.selectionText);
 		}
 	}
 })
